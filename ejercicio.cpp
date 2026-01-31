a6612b97519957f6ec4f79ac04c0fec894f1aac7
danielgarcia-j <00014325@uca.edu.sv>
2026-01-31 14:10:54 -0600

    feat: funciones para mostrar lista adelante y atras

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -1,6 +1,54 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main (){
+struct Nodo {
+    int id;
+    string nombre;
+    float peso;
+    Nodo* sig;
+    Nodo* ant;
+};
 
+
+void mostrarAdelante(Nodo* head) {
+    if (head == nullptr) {
+        cout << "La lista esta vacia\n";
+        return;
+    }
+
+    Nodo* aux = head;
+    while (aux != nullptr) {
+        cout << "ID: " << aux->id
+             << " | Nombre: " << aux->nombre
+             << " | Peso: " << aux->peso << endl;
+        aux = aux->sig;
+    }
+}
+
+void mostrarAtras(Nodo* tail) {
+    if (tail == nullptr) {
+        cout << "La lista esta vacia\n";
+        return;
+    }
+
+    Nodo* aux = tail;
+    while (aux != nullptr) {
+        cout << "ID: " << aux->id
+             << " | Nombre: " << aux->nombre
+             << " | Peso: " << aux->peso << endl;
+        aux = aux->ant;
+    }
+}
+
+
+int contarPaquetes(Nodo* head) {
+    int contador = 0;
+    Nodo* aux = head;
+
+    while (aux != nullptr) {
+        contador++;
+        aux = aux->sig;
+    }
+    return contador;
 }
